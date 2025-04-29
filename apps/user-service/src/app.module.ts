cc7debe433f9633efa76e581a7d63a9b5ce773e8
Edy <46397561+edysupardi@users.noreply.github.com>
2025-04-29 23:41:04 +0700

    feat: set prisma config

--- apps/user-service/src/app.module.ts
+++ apps/user-service/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { UserModule } from './user/user.module';
-import { CommonConfigModule } from '../../../libs/common/config/config.module';
+import { CommonConfigModule } from '@libs/common/config/config.module';
 import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from '@libs/prisma/prisma.module';
 
 @Module({
-    imports: [CommonConfigModule, UserModule, AuthModule],
-    controllers: [],
-    providers: [],
+  imports: [
+    CommonConfigModule,
+    UserModule,
+    AuthModule,
+    PrismaModule
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
